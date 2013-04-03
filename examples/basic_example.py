a91445cf49aea79314b1dbffc941b9ebfcaf39fe
Michael <m.rochester93@googlemail.com>
2013-04-03 08:18:20 +0100

    bit more house work to clean up the root directory

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -4,10 +4,10 @@ import pypalgame as pal
 
 pal.init()
 
-terain =  pal.body.Terrain.create((0,0,0),50)
-box = pal.body.Box.create((0,5,0,1,1,1),mass = 1)
+terain =  pal.body.Terrain((0,0,0),50)
+box = pal.body.Box((0,5,0,1,1,1),mass = 1)
 
-for x in range(5):
+for x in range(25):
     pal.update(0.02)
     print "Current box position is %6.5f at time %4.2f" % \
           (box.get_position()[1],pal.get_time())
